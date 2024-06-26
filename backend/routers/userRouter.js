317a97795c9bcfda7fffb8cbf7d70173808418a8
huyiteng <yitengh2@illinois.edu>
2024-06-25 19:01:20 -0500

    Update userRouter.js

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const { createUser, getAllUser, fetchUserById, getUserById, updatUserById } = require('../controllers/userController');
+
+const {
+    createUser,
+    getAllUser,
+    fetchUserById,
+    getUserById,
+    updatUserById
+} = require('../controllers/userController')
 
 const router = express.Router();
 
+router.post('/', createUser);
 router.get('/', getAllUser);
 router.get('/:id', fetchUserById, getUserById);
 router.patch('/:id', fetchUserById, updatUserById);
-router.post('/', createUser);
 
 module.exports = router;
