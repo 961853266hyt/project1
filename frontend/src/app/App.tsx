481c8db7e914392c314557782a7586aee5dfda71
huyiteng <yitengh2@illinois.edu>
2024-06-29 22:28:17 -0500

    update router

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -4,6 +4,7 @@ import Home from './Home';
 import Cart from './cart/page';
 import Layout from '../components/Layout';
 import FakeLinks from '../components/FakeLinks';
+import AuthForm from '../components/AuthForm';
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,9 @@ const App: React.FC = () => {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/fakelinks" element={<FakeLinks />} />
+          <Route path="/signin" element={<AuthForm />} />
+          <Route path="/signup" element={<AuthForm />} />
+          <Route path="/update-password" element={<AuthForm />} />
         </Routes>
       </Layout>
     </Router>
