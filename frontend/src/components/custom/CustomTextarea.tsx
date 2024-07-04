dab8e783c7b69c16136cbd9362bf43ccf85cfd85
Jiayun Yan <Anyayan168@gmail.com>
2024-07-04 15:02:13 -0400

    fix: description height

--- frontend/src/components/custom/CustomTextarea.tsx
+++ frontend/src/components/custom/CustomTextarea.tsx
@@ -7,7 +7,7 @@ const CustomTextarea: React.FC<{ label: string; name: string }> = ({ label, ...p
   return (
     <div className="mb-4">
       <label className="block text-main-grey">{label}</label>
-      <textarea {...field} {...props} className={`w-full px-4 py-2 border rounded mt-1 ${errorClass}`} />
+      <textarea {...field} {...props} className={`h-[135px] w-full px-4 py-2 border rounded mt-1 ${errorClass}`} />
       {meta.touched && meta.error ? (
         <div className="text-error-orange text-sm mt-1 text-right">{meta.error}</div>
       ) : null}
