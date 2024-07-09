00ccce88a2f61d969bc033c584fb677094254cfa
huyiteng <yitengh2@illinois.edu>
2024-07-08 19:16:17 -0500

    add ErrorBoundary

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,14 @@ import './styles/index.css'
 import App from '../src/app/App.tsx'
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
+import ErrorBoundary from './components/custom/ErrorBoundary.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>,
 )
